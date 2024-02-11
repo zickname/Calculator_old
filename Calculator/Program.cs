6f1e94c16292629853f1269a56212ec58eef5546
Aydar U. <aidohha@gmail.com>
2024-02-11 23:14:15 +0600

    refactoring

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using static System.Globalization.CultureInfo;
 
 namespace Calculator
 {
@@ -13,79 +12,93 @@ namespace Calculator
             Multiplication,
             Division
         }
-        
+
         static void Main()
         {
             while (true)
             {
-                double firstNumber;
-                double secondNumber;
-                double result;
-                char sign;
-                string mathOperators = "+-*/";
-                MathOperation operation;
-
-                Console.WriteLine("Введите первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
-                Console.Write("Введите первое число: ");
-
-                while (!double.TryParse(Console.ReadLine(),NumberStyles.Float,CurrentCulture, out firstNumber))
+                try
                 {
-                    Console.WriteLine("Ошибка. Введите корректное число.");
-                    Console.Write("Введите первое число: ");
-                }
+                    double firstNumber = GetValidNumber("Введите первое число: ");
+                    char sign = GetValidOperator();
+                    double secondNumber = GetValidNumber("Введите второе число: ");
 
+                    MathOperation operation = GetMathOperation(sign);
+
+                    double result = PerformOperation(firstNumber, secondNumber, operation);
+
+                    Console.WriteLine($"Результат: {result}");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                    Console.WriteLine("Попробуйте еще раз.");
+                }
+            }
+        }
+
+        static double GetValidNumber(string message)
+        {
+            double number;
+            Console.Write(message);
+
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Float, CultureInfo.CurrentCulture, out number))
+            {
+                Console.WriteLine("Ошибка. Введите корректное число.");
+                Console.Write(message);
+            }
+
+            return number;
+        }
+
+        static char GetValidOperator()
+        {
+            string validOperators = "+-*/";
+            Console.Write("Введите знак действия ( +, -, *, / ): ");
+            char sign = Console.ReadKey().KeyChar;
+
+            while (validOperators.IndexOf(sign) == -1)
+            {
+                Console.WriteLine("\nОшибка. Вы ввели неверный знак.");
                 Console.Write("Введите знак действия ( +, -, *, / ): ");
-                
-                sign = Convert.ToChar(Console.ReadKey().KeyChar);
+                sign = Console.ReadKey().KeyChar;
+            }
 
-                while (!mathOperators.Contains(sign.ToString()))
-                {
-                    Console.WriteLine("\nОшибка. Вы ввели неверный знак.");
-                    Console.Write("Введите знак действия ( +, -, *, / ): ");
+            Console.Write("\n");
+            return sign;
+        }
 
-                    sign = Convert.ToChar(Console.ReadKey().KeyChar);
-                }
+        static MathOperation GetMathOperation(char sign)
+        {
+            switch (sign)
+            {
+                case '+': return MathOperation.Addition;
+                case '-': return MathOperation.Subtraction;
+                case '*': return MathOperation.Multiplication;
+                case '/': return MathOperation.Division;
+                default: throw new ArgumentException("Неверный знак действия");
+            }
+        }
 
-                operation = (MathOperation)mathOperators.IndexOf(sign);
-                
-                Console.Write("\nВведите Второе число: ");
-
-                while (!double.TryParse(Console.ReadLine(),NumberStyles.Float, CurrentCulture, out secondNumber))
-                {
-                    Console.WriteLine("Ошибка. Введите корректное число.");
-                    Console.Write("Введите Второе число: ");
-                }
-
-                switch (operation)
-                {
-                    case MathOperation.Addition:
-                        result = firstNumber + secondNumber;
-                        Console.WriteLine("Сумма ваших чисел равна " + result);
-                        break;
-                    case MathOperation.Subtraction:
-                        result = firstNumber - secondNumber;
-                        Console.WriteLine("Разность ваших чисел равна " + result);
-                        break;
-                    case MathOperation.Multiplication:
-                        result = firstNumber * secondNumber;
-                        Console.WriteLine("Произведение ваших чисел равно " + result);
-                        break;
-                    case MathOperation.Division:
-                        if (secondNumber == 0)
-                        {
-                            Console.WriteLine("Ошибка. Делитель не может быть равным нулю.");
-                            break;
-                        }
-                        else
-                        {
-                            result = firstNumber / secondNumber;
-                            Console.WriteLine("Частное ваших чисел равна " + result);
-                            break;
-                        }
-                    default:
-                        Console.WriteLine("Ошибка. Вы ввели неверный знак.");
-                        break;
-                }
+        static double PerformOperation(double firstNumber, double secondNumber, MathOperation operation)
+        {
+            switch (operation)
+            {
+                case MathOperation.Addition:
+                    return firstNumber + secondNumber;
+                case MathOperation.Subtraction:
+                    return firstNumber - secondNumber;
+                case MathOperation.Multiplication:
+                    return firstNumber * secondNumber;
+                case MathOperation.Division:
+                    if (secondNumber == 0)
+                    {
+                        throw new DivideByZeroException("Ошибка. Делитель не может быть равным нулю.");
+                    }
+                    return firstNumber / secondNumber;
+                default:
+                    throw new ArgumentException("Неверный знак действия");
             }
         }
     }
