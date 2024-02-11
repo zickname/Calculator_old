5084155bcf1dc57d930ca2df1d6e45a9ebf1ca00
Aydar U. <aidohha@gmail.com>
2024-02-11 21:06:56 +0600

    using enum update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,70 +1,76 @@
 ﻿using System;
 using System.Globalization;
+using static System.Globalization.CultureInfo;
+
 namespace Calculator
 {
     internal class Program
     {
         enum MathOperation
         {
-            Addition = '+',
-            Subtraction = '-',
-            Multiplication = '*',
-            Division = '/'
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
         }
+        
         static void Main()
         {
-            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CurrentCulture;
             while (true)
             {
                 double firstNumber;
                 double secondNumber;
                 double result;
                 char sign;
+                string mathOperators = "+-*/";
+                MathOperation operation;
 
                 Console.WriteLine("Введите первое число, затем знак действия(+,-,*,/), которое хотите совершить и второе число.");
                 Console.Write("Введите первое число: ");
 
-                while (!double.TryParse(Console.ReadLine(), out firstNumber))
+                while (!double.TryParse(Console.ReadLine(),NumberStyles.Float,CurrentCulture, out firstNumber))
                 {
                     Console.WriteLine("Ошибка. Введите корректное число.");
                     Console.Write("Введите первое число: ");
                 }
 
-                Console.Write("Введите знак действия: ");
+                Console.Write("Введите знак действия ( +, -, *, / ): ");
                 
-                sign = Convert.ToChar(Console.ReadLine() ?? string.Empty);
+                sign = Convert.ToChar(Console.ReadKey().KeyChar);
 
-                while (!Enum.IsDefined(typeof(MathOperation), (int)sign))
+                while (!mathOperators.Contains(sign.ToString()))
                 {
-                    Console.WriteLine("Ошибка. Вы ввели неверный знак.");
-                    Console.Write("Введите знак действия: ");
+                    Console.WriteLine("\nОшибка. Вы ввели неверный знак.");
+                    Console.Write("Введите знак действия ( +, -, *, / ): ");
 
-                    sign = Convert.ToChar(Console.ReadLine() ?? string.Empty);
+                    sign = Convert.ToChar(Console.ReadKey().KeyChar);
                 }
 
-                Console.Write("Введите Второе число: ");
+                operation = (MathOperation)mathOperators.IndexOf(sign);
+                
+                Console.Write("\nВведите Второе число: ");
 
-                while (!double.TryParse(Console.ReadLine(), out secondNumber))
+                while (!double.TryParse(Console.ReadLine(),NumberStyles.Float, CurrentCulture, out secondNumber))
                 {
                     Console.WriteLine("Ошибка. Введите корректное число.");
                     Console.Write("Введите Второе число: ");
                 }
 
-                switch (sign)
+                switch (operation)
                 {
-                    case '+':
+                    case MathOperation.Addition:
                         result = firstNumber + secondNumber;
                         Console.WriteLine("Сумма ваших чисел равна " + result);
                         break;
-                    case '-':
+                    case MathOperation.Subtraction:
                         result = firstNumber - secondNumber;
                         Console.WriteLine("Разность ваших чисел равна " + result);
                         break;
-                    case '*':
+                    case MathOperation.Multiplication:
                         result = firstNumber * secondNumber;
                         Console.WriteLine("Произведение ваших чисел равно " + result);
                         break;
-                    case '/':
+                    case MathOperation.Division:
                         if (secondNumber == 0)
                         {
                             Console.WriteLine("Ошибка. Делитель не может быть равным нулю.");
